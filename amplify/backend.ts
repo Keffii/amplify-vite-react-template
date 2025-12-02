e7bb933e0a58d9c4cad25af08f0a5bcb47b56117
Kevin <kevinbogosta.vallin@yh.nackademin.se>
2025-12-02 11:21:28 +0100

    revert: remove Timestream integration (not in free tier)

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,12 +4,10 @@ import { data } from './data/resource';
 
 import { graphqlIoTCoreButtonEvents } from './functions/graphqlIoTCoreButtonEvents/resource';
 import { graphqlIoTCoreStatus } from './functions/graphqlIoTCoreStatus/resource';
-import { dynamodbToTimestream } from './functions/dynamodbToTimestream/resource';
 
 defineBackend({
   auth,
   data,
   graphqlIoTCoreButtonEvents,
   graphqlIoTCoreStatus,
-  dynamodbToTimestream,
 });
